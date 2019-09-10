ff196e8905e0e063309403b0267a0b5948ceac35
admin <admin@itheima.com>
2019-09-10 09:06:19 +0800

    1

---
+++ Hello3.java
@@ -0,0 +1,15 @@
+
+
+public Class Hello{
+	private String username;
+	private String password;
+	
+	public static void main(String[] args){
+		System.out.println("Hello World!")
+	}
+	
+	public void test(){
+		int i = 9;
+		i++;
+	}
+}
